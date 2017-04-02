1cd0efb0a2082fd4e2cb5b084153bdef9502d2ad
Ubuntu <ubuntu@ip-172-31-9-153.us-west-2.compute.internal>
2017-04-02 22:45:48 +0000

    make new process fork for each site

--- code/scraper/auto_scrape.py
+++ code/scraper/auto_scrape.py
@@ -7,7 +7,7 @@ import nltk
 from random import randint
 from time import sleep
 import json
-import sys
+import sys, os
 from custom_logging import get_logger 
 
 def scrape():
@@ -41,75 +41,79 @@ def scrape():
 		logging.error("::Exception: " + str(e) + "::")
 		sys.exit(1)
 	for site in site_list:
-		name = site['name']
-		url = site['url']
-		# if a site should throw an exception for any reason (maybe bad url), catch it, pass, and move to next site
-		try:
-			paper = newspaper.build(url,
-									keep_article_html=True,
-									fetch_images=False,
-									memoize_articles=True,	# track the articles we have already scraped from session to session
-									MIN_WORD_COUNT=200,		# this doesn't seem to be having any affect
-									number_threads=2,
-									request_timeout=12,
-									thread_timeout=3)
-			total = paper.size()
-			numScraped = total
-			authors = []
-			secondAuth = ''
-			
-			for x in range(0,numScraped):
-				sleep(randint(3,6))
-				if paper.articles[x] != None:
-					paper.articles[x].download()
-					if paper.articles[x].is_downloaded:
-						if paper.articles[x].html != None:
-							paper.articles[x].parse()
-							# Ensure article is long enough to be valid
-							if len(nltk.word_tokenize(paper.articles[x].text)) > 200:
-								html = paper.articles[x].article_html.replace('\n', ' ')
-								title = (paper.articles[x].title)
-								url = paper.articles[x].url
-								published_date = paper.articles[x].publish_date
-								authors = paper.articles[x].authors
-								text = paper.articles[x].text.replace('\n', ' ')
-								paper.articles[x].nlp()
-								keywords = paper.articles[x].keywords
-								summary = paper.articles[x].summary
-								try:
-									primary_author = str(authors[0])
-								except IndexError as ie:
-									primary_author = None
-									pass
-								# if the secondary authors list is too long there was some sort of parsing problem, so throw them away to avoid insertion errors
-								try:
-									if sum([len(author) for author in authors[1:]]) > 100:
-										secondAuth = ''
-									else:
-										secondAuth = str(authors[1:])
-								except IndexError as ie:
-									pass
-								try:
-									insert = table.insert().values( 
-										site=name, 
-										title=title, 
-										author=primary_author,
-										secondary_authors=secondAuth,
-										published_on=published_date,
-										accessed_on=func.current_timestamp(),
-										url=url,
-										body=text,
-										html=html,
-										newspaper_keywords=keywords,
-										newspaper_summary= 	
-										summary)					
-									engine.execute(insert)
-								except Exception as e:
-									logging.error("::Exception:Database insertion error for page: " + url + "::")
-									logging.error("::Exception: " + str(e) + "::")
-									pass
-		except Exception as e:
-			logging.error("::Exception:Scrapping error for site: " + name + "::")
-			logging.error("::Exception: " + str(e) + "::")
-			pass
+		child_id = os.fork()
 
+		if child_id == 0:			
+			name = site['name']
+			url = site['url']
+			# if a site should throw an exception for any reason (maybe bad url), catch it, pass, and move to next site
+			try:
+				paper = newspaper.build(url,
+										keep_article_html=True,
+										fetch_images=False,
+										memoize_articles=True,	# track the articles we have already scraped from session to session
+										MIN_WORD_COUNT=200,		# this doesn't seem to be having any affect
+										number_threads=2,
+										request_timeout=12,
+										thread_timeout=3)
+				total = paper.size()
+				numScraped = total
+				authors = []
+				secondAuth = ''
+			
+				for x in range(0,numScraped):
+					sleep(randint(3,6))
+					if paper.articles[x] != None:
+						paper.articles[x].download()
+						if paper.articles[x].is_downloaded:
+							if paper.articles[x].html != None:
+								paper.articles[x].parse()
+								# Ensure article is long enough to be valid
+								if len(nltk.word_tokenize(paper.articles[x].text)) > 200:
+									html = paper.articles[x].article_html.replace('\n', ' ')
+									title = (paper.articles[x].title)
+									url = paper.articles[x].url
+									published_date = paper.articles[x].publish_date
+									authors = paper.articles[x].authors
+									text = paper.articles[x].text.replace('\n', ' ')
+									paper.articles[x].nlp()
+									keywords = paper.articles[x].keywords
+									summary = paper.articles[x].summary
+									try:
+										primary_author = str(authors[0])
+									except IndexError as ie:
+										primary_author = None
+										pass
+									# if the secondary authors list is too long there was some sort of parsing problem, so throw them away to avoid insertion errors
+									try:
+										if sum([len(author) for author in authors[1:]]) > 100:
+											secondAuth = ''
+										else:
+											secondAuth = str(authors[1:])
+									except IndexError as ie:
+										pass
+									try:
+										insert = table.insert().values( 
+											site=name, 
+											title=title, 
+											author=primary_author,
+											secondary_authors=secondAuth,
+											published_on=published_date,
+											accessed_on=func.current_timestamp(),
+											url=url,
+											body=text,
+											html=html,
+											newspaper_keywords=keywords,
+											newspaper_summary= 	
+											summary)					
+										engine.execute(insert)
+									except Exception as e:
+										logging.error("::Exception:Database insertion error for page: " + url + "::")
+										logging.error("::Exception: " + str(e) + "::")
+										pass
+			except Exception as e:
+				logging.error("::Exception:Scrapping error for site: " + name + "::")
+				logging.error("::Exception: " + str(e) + "::")
+				pass
+		else:
+			pid, status = os.waitpid(child_id, 0)
