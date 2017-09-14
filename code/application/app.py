96fb3cd2f0bb714de33785162c68b7eafe04c2f3
Zach Bryant <ztbc68@mail.missouri.edu>
2017-09-14 12:18:54 -0500

    Removed login stuff and added flask-bootstrap

--- code/application/app.py
+++ code/application/app.py
@@ -1,26 +1,16 @@
 from flask import Flask, render_template, session, request
-
+from flask_bootstrap import Bootstrap
 import os
 
 app = Flask(__name__)
+Bootstrap(app)
 
 app.secret_key = os.urandom(12) 
 
 
 @app.route('/')
 def home():
-	if not session.get('logged_in'):
-		return render_template('login.html')
-	else:
-		return render_template('index.html')
-
-@app.route('/login', methods=['POST'])
-def do_admin_login():
-    if request.form['password'] == 'password' and request.form['username'] == 'admin':
-        session['logged_in'] = True
-    else:
-        flash('wrong password!')
-    return home()
+	return render_template('index.html')
 
 @app.route('/test')
 def test_route():
