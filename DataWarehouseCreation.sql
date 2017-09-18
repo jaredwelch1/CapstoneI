74e564a973456d17a524a3248c52662b587d8634
Ali Raza <aardz6@mail.missouri.edu>
2017-09-18 10:21:44 -0500

    Update ddl for data warehous

--- DataWarehouseCreation.sql
+++ DataWarehouseCreation.sql
@@ -1,75 +1,66 @@
-create table autor_dim(
-	author_id	SERIAL,
-	name	TEXT	
+create table author_dim(
+    author_id SERIAL,
+    name TEXT,
+    PRIMARY KEY(author_id)
 );
 
 create table site_dim(
-	site_id SERIAL, 
-	domain  text, 
-	name	text, 
-	supersite_id int	
+    site_id SERIAL, 
+    domain text, 
+    name text, 
+    supersite_id int,
+    PRIMARY KEY(site_id)
 );
 
 create table article_dim(
-	article_id 			SERIAL, 
-	title				text, 
-	URL					text, 
-	published_date		timestamp,
-	accessed_date		timestamp, 
-	body				text,
-	html				text, 
-	keywords			text, 
-	summary				text
+    article_id SERIAL, 
+    title text, 
+    URL text, 
+    published_date timestamp,
+    accessed_date timestamp, 
+    body text,
+    html text, 
+    keywords text, 
+    summary text,
+    PRIMARY KEY(article_id)
 );
 
-create table example_stat_dim1(
-	statistic_id	SERIAL,
-	NLP_results		JSON
-);
-create table example_stat_dim2(
-	
+create table statistic_dim(
+    statistic_id SERIAL,
+    NLP_results JSON,
+    PRIMARY KEY(statistic_id)
 );
 
 create table fact(
-	fact_id SERIAL,
-	article_id	int references article_dim(article_id),
-	author_id	int references author_dim(author_id),
-	site_id	int references site_dim(site_id),
-	statistic_id	int references statistic_dim(statistic_id),
+    fact_id SERIAL,
+    article_id int,
+    author_id int,
+    site_id int,
+    statistic_id int,
+    FOREIGN KEY (article_id) REFERENCES article_dim(article_id),
+    FOREIGN KEY (author_id) REFERENCES author_dim(author_id),
+    FOREIGN KEY (site_id) REFERENCES site_dim(site_id),
+    FOREIGN KEY (statistic_id) REFERENCES statistic_dim(statistic_id)
 );
 
-
-insert into author_dim
+insert into author_dim (name)
 select distinct author 
-from scraped;
-
-insert into site_dim
-select distinct site as name, url as domain, 
-parse(url) as supersite id
-from scraped;
-
-insert into article_dim
-select title, url, published_date, accessed_date, body, html, keywords, summary
-from scraped;
-
-insert into fact 
-select article_id, author_id, site_id
-from various tables, 
-then commit; 
+from articles;
 
 
+-- Need to define parse function
+-- insert into site_dim (name, domain, supersite_id)
+-- select distinct site as name, url as domain, 
+-- parse(url) as supersite_id
+-- from articles;
 
+-- Date timestamp messing things
+-- insert into article_dim (title, URL,published_date, accessed_date, body, html, keywords, summary)
+-- select title, url, published_date, accessed_date, body, html, keywords, summary
+-- from articles;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- Waiting on previous tables before we run this
+-- insert into fact (article_id, author_id, site_id)
+-- select article_id, author_id, site_id
+-- from various tables, 
+-- then commit; 
