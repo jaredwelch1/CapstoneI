8cf0cb7a3e6ffce1552a75974444430455684994
Ali Raza <aardz6@mail.missouri.edu>
2017-09-20 09:54:02 -0500

    Update DataWarehouseCreation script, add external connection info, and reorganize repo

--- DataWarehouse/DataWarehouseCreation.sql
+++ DataWarehouse/DataWarehouseCreation.sql
@@ -42,25 +42,3 @@ create table fact(
     FOREIGN KEY (site_id) REFERENCES site_dim(site_id),
     FOREIGN KEY (statistic_id) REFERENCES statistic_dim(statistic_id)
 );
-
-insert into author_dim (name)
-select distinct author 
-from articles;
-
-
--- Need to define parse function
--- insert into site_dim (name, domain, supersite_id)
--- select distinct site as name, url as domain, 
--- parse(url) as supersite_id
--- from articles;
-
--- Date timestamp messing things
--- insert into article_dim (title, URL,published_date, accessed_date, body, html, keywords, summary)
--- select title, url, published_date, accessed_date, body, html, keywords, summary
--- from articles;
-
--- Waiting on previous tables before we run this
--- insert into fact (article_id, author_id, site_id)
--- select article_id, author_id, site_id
--- from various tables, 
--- then commit; 
